385f0441e6aa927576ce95fffdc8425ced8eceb4
Maksymilian Czarnecki <maksymilian@czarnecki.me>
2023-01-21 18:28:14 +0100

    Added search functionality.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  Observable,
+  Subscription,
+  tap,
+} from 'rxjs';
 import { Budget } from '../common/interfaces/budget';
-import { ExpensesService } from '../common/services/expenses.service';
+import { BudgetsService } from '../common/services/budgets.service';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +22,36 @@ import { ExpensesService } from '../common/services/expenses.service';
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   public readonly expenses$: Observable<Budget[]>;
+  public readonly search: FormControl = new FormControl('');
+  public readonly searchLoading$: BehaviorSubject<boolean> =
+    new BehaviorSubject(false);
+  private search$: Subscription;
 
-  constructor(private expensesService: ExpensesService) {
-    this.expenses$ = this.expensesService.expenses$;
+  constructor(private budgetsService: BudgetsService) {
+    this.expenses$ = this.budgetsService.expenses$.pipe(
+      tap(() => {
+        this.searchLoading$.next(false);
+      })
+    );
+  }
+
+  ngOnInit() {
+    this.search$ = this.search.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        tap((searchString) => {
+          this.searchLoading$.next(true);
+          this.budgetsService.setSearchString(searchString);
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.budgetsService.setSearchString('');
+    this.search$.unsubscribe();
   }
 }
