f22c76b852241686da87820e63fab7a58a18ffc3
Maksymilian Czarnecki <maksymilian@czarnecki.me>
2023-01-22 18:14:56 +0100

    Always redirect from '' to '/home'.

--- src/app/common/state/effects/budget.effects.ts
+++ src/app/common/state/effects/budget.effects.ts
@@ -1,24 +1,37 @@
 import { Injectable } from '@angular/core';
 import { createEffect } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { EMPTY, of } from 'rxjs';
+import { map, mergeMap, switchMap } from 'rxjs/operators';
+import { AuthService } from '../../services/auth.service';
 import { BudgetsService } from '../../services/budgets.service';
 
 @Injectable()
 export class BudgetEffects {
   budgets$ = createEffect(() =>
-    this.budgetsService.getAll().pipe(
-      mergeMap((actions) => actions),
-      map((action) => {
-        return {
-          type: `[Budgets Data] ${action.type}`,
-          payload: {
-            id: action.payload.doc.id,
-            ...action.payload.doc.data(),
-          },
-        };
+    this.authService.userToken$.pipe(
+      switchMap((token: string) => {
+        if (!token) {
+          return EMPTY;
+        }
+
+        return this.budgetsService.getAll().pipe(
+          mergeMap((actions) => actions),
+          map((action) => {
+            return {
+              type: `[Budgets Data] ${action.type}`,
+              payload: {
+                id: action.payload.doc.id,
+                ...action.payload.doc.data(),
+              },
+            };
+          })
+        );
       })
     )
   );
 
-  constructor(private budgetsService: BudgetsService) {}
+  constructor(
+    private budgetsService: BudgetsService,
+    private authService: AuthService
+  ) {}
 }
